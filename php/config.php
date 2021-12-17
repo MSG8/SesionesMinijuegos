499ffe6e7d7afb361bcd66ee555ea69bddebe2ae
Manuel <91004349+MSG8@users.noreply.github.com>
2021-12-17 14:02:25 +0100

    version1.4, cambio a base de datos a heidi

---
+++ php/config.php
@@ -0,0 +1,16 @@
+<?php
+  /**
+   * Este archivo tendra la informacion para nuestra conexion, usaremos constantes para esto
+   */
+    // //Para nuestro servidor FTP
+    // define ('SERVIDOR', '13.2daw.esvirgua.com');
+    // define ('USUARIO', 'user2daw_13');
+    // define ('CONTRASENIA', 'k8D~R$8plhis');
+    // define ('BASEDATOS', 'user2daw_BD1-13'); 
+
+    //PARA EL LOCALHOST
+    define ('SERVIDOR', '13.2daw.esvirgua.com');
+    define ('USUARIO', 'user2daw_13');
+    define ('CONTRASENIA', 'k8D~R$8plhis');
+    define ('BASEDATOS', 'user2daw_BD1-13');
+?>
